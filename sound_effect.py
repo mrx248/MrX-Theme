85cca8455c37f595e549dd5a16eb0b700620f38b
mrx <166585689+mrx248@users.noreply.github.com>
2024-04-12 13:25:30 +0530

    Add files via upload

---
+++ sound_effect.py
@@ -0,0 +1,15 @@
+import os 
+num=1
+print(num)
+os.system("clear")
+print("\033[1m\033[33m[\033[32m─────────────────Booting Termux Terminal─────────────────\033[33m]\033[0m")
+
+
+# Condition 
+
+if num == 1:
+    os.system("mpv Access-Granted.mp3")
+else :
+    print("audio not found")
+ 
+###########################################
